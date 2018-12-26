7b721327e00eaf4b3ff68a5c39b973918a709163
Haruka Takahashi <h173335@edu.tut.ac.jp>
2018-12-26 09:54:41 +0900

    [add] graph

--- app.py
+++ app.py
@@ -13,14 +13,20 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # HTMLの外観を定義
 app.layout = html.Div([
     html.H1(children="App title"),
-    # テキストボックス
-    dcc.Input(id="input-1", type="text", size=70),
+    html.Div([
+        # テキストボックス
+        dcc.Input(id="input-1", type="text", size=70),
+        # テキストボックス
+        dcc.Input(id="input-2", type="text", size=20),
+    ], style={'display': 'inline-block'}),
     # 送信ボタン
     html.Button(id="submit-button", children="Submit"),
     # 余白
     html.Div(style={'padding': 10}),
     # 表
     html.Div(id="table-area"),
+    # グラフ
+    dcc.Graph(id="graph"),
     # データ格納エリア(非表示)
     html.Div(id='intermediate-value', style={'display': 'none'})
 ], style={'padding': 50})
@@ -30,13 +36,14 @@ app.layout = html.Div([
 @app.callback(
     Output('intermediate-value', 'children'),
     [Input("submit-button", "n_clicks")],
-    [State("input-1", "value")]
+    [State("input-1", "value"), State("input-2", "value")]
 )
-def clean_data(n_clicks, input_text):
+def clean_data(n_clicks, input_text, input_text2):
     # データ表示　任意のメソッドに置き換える（メソッドの戻り値はpandasデータフレーム型であること）
-    df = sample_data(input_text)
+    df = sample_data(input_text, float(input_text2))
 
     return df.to_json(date_format='iso', orient='split')
+
 
 ## 格納エリアの変化を受けて、テーブルを描画 ##
 @app.callback(
@@ -53,39 +60,36 @@ def update_table(jsonified_cleaned_data):
         data=df.to_dict("rows"),                # データ（行）
         style_table={'width': '100%'},
         style_data={'whiteSpace': 'normal'},
-        # 幅等の設定
-        # style_cell_conditional=[
-        #     {'if': {'column_id': 'order'},
-        #         'width': '5%',
-        #         'textAlign': 'center',
-        #     },
-        #     {'if': {'column_id': 'tag'},
-        #         'width': '30%',
-        #         'textAlign': 'left',
-        #     },
-        #     {'if': {'column_id': 'text'},
-        #         'textAlign': 'left',
-        #     },
-        #     {'if': {'column_id': 'value'},
-        #         'width': '15%',
-        #         'textAlign': 'left',
-        #     },
-        # ],
         content_style='grow',
-        # css=[{
-        #     'selector': '.dash-cell div.dash-cell-value',
-        #     'rule': 'display: inline; white-space: inherit; overflow: inherit; text-overflow: inherit;'
-        # }],
-        # style_header={
-        #     'backgroundColor': 'white',
-        #     'fontWeight': 'bold'
-        # },
     ),
 
 
-### サンプルデータ表示用
-def sample_data(input_text):
-    data = [ ["a", 0.9], ["b", 0.8], ["c", 0.7], ["d", 0.6], ["e", 0.5], [input_text, 0.1] ]
+## 格納エリアの変化を受けて、グラフを描画 ##
+@app.callback(
+    Output("graph", "figure"),
+    [Input('intermediate-value', 'children')]
+)
+def update_graph(jsonified_cleaned_data):
+    df = pd.read_json(jsonified_cleaned_data, orient='split')
+
+    data = {
+            "x": list(range(len(df))),
+            "y": df["value"].values.tolist(),
+            "type": "lines+markers",
+            "name": "サンプル０１",
+            "text": df["tag"].values.tolist(),
+            "hover": "text",
+    },
+    return {
+        "data": data,
+        "layout": {
+            "title": "サンプル"
+        }
+    }
+
+### サンプルデータ 表示用 ###
+def sample_data(input_text, value):
+    data = [ ["a", 0.9], ["b", 0.8], ["c", 0.7], ["d", 0.6], ["e", 0.5], [input_text, value] ]
     df = pd.DataFrame(data, columns=["tag", "value"])
     return df
 
